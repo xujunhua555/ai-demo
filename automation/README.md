0e5c83c2515e40e2de1a47e2d4982dad0b1d8fd7
xujunhua555 <18409916+xujunhua555@users.noreply.github.com>
2026-03-04 17:25:24 +0800

    Add configuration and automation files

---
+++ automation/README.md
@@ -0,0 +1,14 @@
+# Automation Scripts
+
+自动化脚本目录
+
+## 功能
+
+- 获取团队成员信息
+- 自动发送周报
+
+## 使用方法
+
+```bash
+python get_members_simple.py
+```
--- frontend/src/utils/dayjs.js
+++ frontend/src/utils/dayjs.js
@@ -1,5 +1,17 @@
-import './dayjs.js'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+import advancedFormat from 'dayjs/plugin/advancedFormat'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+import weekYear from 'dayjs/plugin/weekYear'
+import isoWeek from 'dayjs/plugin/isoWeek'
 
-export default {
-  dayjs: () => import('dayjs')
-}
+dayjs.extend(advancedFormat)
+dayjs.extend(customParseFormat)
+dayjs.extend(weekOfYear)
+dayjs.extend(weekYear)
+dayjs.extend(isoWeek)
+
+dayjs.locale('zh-cn')
+
+export default dayjs
